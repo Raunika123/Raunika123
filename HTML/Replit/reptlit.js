263bd2e6a93fc1d491ae9f0d09eb8e991c9af99c
Raunika Malhotra <malhotrar@student.suttonschools.net>
2022-07-24 15:35:59 -0400

    Adding multipal projects

---
+++ HTML/Replit/reptlit.js
@@ -0,0 +1,31 @@
+//alert('hi');
+//var name="Raunika Malhotra";
+//alert(name);
+//console.log(name);
+//console.log("My name is" + name);
+var firstname="Raunika";
+var lastname="Malhotra";
+//var fullname=firstname+lastname;
+//console.log(fullname);
+var fullname=function(){
+  return fullname+"-"lastname;
+}
+console.log(fullname())
+var firstname="pine";
+var lastname="apple";
+console.log(fullname())
+//alert('Raunika');
+//var name="Raunika Malhotra";
+//alert(name);
+//var firstname="Raunika";
+//var lastname="Malhotra";
+//var fullname=firstname+lastname;
+//alert(fullname);
+//alert('I love birds because they are very pretty and colorful.')
+//var g="I love rabbits";
+//var f= " because they are soft.";
+//var p= g+f;
+//alert(p);
+var firstname="Raunika";
+var lastname="Malhotra";
+
